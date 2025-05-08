fe5e5ecfa5d7dfc1d94fc6609eb718f078c612f1
Lucia Acuña <luciaacunaaaa@gmail.com>
2025-05-08 09:01:27 -0300

    Agregué más ejercicios

--- Sakila.sql
+++ Sakila.sql
@@ -24,5 +24,50 @@ SELECT max (return_date), min (return_date), f.title FROM film f
 INNER JOIN inventory i ON f.film_id= i.film_id
 INNER JOIN rental r ON i.inventory_id= r.inventory_id
 
-/*Ejercicio 5*/
+/*Ejercicio 6*/
+SELECT rating, COUNT(*) AS cantidad_peliculas
+FROM film f
+GROUP BY rating;
 
+/*Ejercicio 7*/
+SELECT count(*) as cant_pelicula, ca.name  
+FROM film f
+INNER JOIN film_category c ON f.film_id = c.film_id
+INNER JOIN category ca ON c.category_id = ca.category_id
+GROUP BY ca.name
+
+/*Ejercicio 8*/
+SELECT a.first_name,a.last_name,count(a.actor_id) as cant_actor, f.title
+FROM film f
+INNER JOIN film_actor fa ON f.film_id = fa.film_id
+INNER JOIN actor a ON fa.actor_id = a.actor_id
+GROUP BY f.film_id
+ORDER by cant_actor desc limit 10
+
+/*Ejercicio 9*/
+SELECT a.address, c.city, co.country,count(i.inventory_id)
+FROM inventory i
+INNER JOIN store s ON i.store_id = s.store_id
+INNER JOIN address a ON s.address_id = a.address_id
+INNER JOIN city c ON a.city_id = c.city_id
+INNER JOIN country co ON c.country_id = co.country_id  
+GROUP by a.address
+
+/*Ejercicio 10*/
+ SELECT a.address, c.city, co.country,count(f.film_id)
+FROM film f
+INNER JOIN inventory i ON f.film_id = i.film_id
+INNER JOIN store s ON i.store_id = s.store_id
+INNER JOIN address a ON s.address_id = a.address_id
+INNER JOIN city c ON a.city_id = c.city_id
+INNER JOIN country co ON c.country_id = co.country_id  
+GROUP by a.address
+
+/*Ejercicio 11*/
+SELECT AVG(f.rental_duration * f.rental_rate ) AS costo_promedio, c.name
+FROM film f
+INNER JOIN film_category fc ON f.film_id = fc.film_id
+INNER JOIN category c ON fc.category_id = c.category_id
+GROUP by c.name
+
+ /*Ejercicio 12*/
